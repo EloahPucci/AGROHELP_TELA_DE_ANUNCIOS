cd510f6d683765a2e7746e0011432e5e02bf2084
Eloah Pucci <eloahpucci@gmail.com>
2018-10-19 21:12:10 -0300

    Commit repositorio inteiro

---
+++ app/src/main/java/com/example/eloah/agrohelp_tela_de_anuncios/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.eloah.agrohelp_tela_de_anuncios;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
